7a12282a8460388683b92e8888626e52f9933ae1
Joel Francisco <joel@pop-os.localdomain>
2021-04-12 12:30:57 -0300

    fix: db user creation arguments

--- src/repositories/implemantation/mongodb.ts
+++ src/repositories/implemantation/mongodb.ts
@@ -1,5 +1,10 @@
 import { User } from "../../entities/User";
+import { ICreateUserRequestDTO } from "../../useCases/CreateUser/CreateUserDTO";
 import { IUserRepository } from "../IUserRepository";
+
+const { MongoClient } = require('mongodb');
+const uri = "";
+const client = new MongoClient(uri);
 
 class UserRepository implements IUserRepository {
   constructor(
@@ -7,7 +12,7 @@ class UserRepository implements IUserRepository {
     private database
   ) {}
 
-  async function findByEmail(id: string) {
+  async findByEmail(id: string) {
     let db_user;
     try { 
       await client.connect();
@@ -19,24 +24,23 @@ class UserRepository implements IUserRepository {
       db_user = await users.findOne(query);
     } finally {
       await client.close();
-      const user = new User(db_user[0].id, db_user[0].name, db_user[0].email, db_user[0].password);
+      const data: ICreateUserRequestDTO = { 
+        name: db_user[0].name,
+        email: db_user[0].email,
+        password: db_user[0].password
+      };
+      const user = new User(data, db_user[0].id);
       return user;
     } 
   }
+
+  async save(new_user: User) {
+    await client.connect();
+
+    const database = client.db('tyt');
+    const users = database.collection('users');
+
+    users.insertOne(new_user);
+  }
 }
 
-const { MongoClient } = require('mongodb');
-
-const uri = "";
-
-const client = new MongoClient(uri);
-
-
-async function save(new_user: User) {
-  await client.connect();
-
-  const database = client.db('tyt');
-  const users = database.collection('users');
-
-  users.insertOne(new_user);
-}
